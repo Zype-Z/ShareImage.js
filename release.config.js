8204c20becda3011e5317adddb36edd98c239316
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-09-13 17:58:40 +0530

    Fix: Fixed Semantic Release

--- release.config.js
+++ release.config.js
@@ -1,22 +1,45 @@
 module.exports = {
-  branches: [
-    "+([0-9])?(.{+([0-9]),x}).x",
-    "master",
-    "next",
-    "next-major",
-    {
-      name: "beta",
-      prerelease: true,
-    },
-    {
-      name: "alpha",
-      prerelease: true,
-    },
-  ],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
-  ],
+    branches: [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "master",
+        "next",
+        "next-major",
+        {
+            name: "beta",
+            prerelease: true,
+        },
+        {
+            name: "alpha",
+            prerelease: true,
+        },
+    ],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "eslint",
+            },
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                preset: "eslint",
+            },
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md"],
+                "message": "${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
+        "@semantic-release/npm",
+        "@semantic-release/github",
+    ]
 };
